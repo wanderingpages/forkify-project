cc5c5b449924e09415109d5a476ab6743dbad71d
wanderingpages <angellaannraju.29@gmail.com>
2025-05-16 00:53:10 +0530

    Added new welcome feature

--- src/js/controller.js
+++ src/js/controller.js
@@ -134,6 +134,10 @@ const controlAddRecipe = async function (newRecipe) {
   }
 };
 
+const newFeature = function () {
+  console.log('Welcome to the application');
+};
+
 const init = function () {
   bookmarksView.addHandlerRender(controlBookmarks);
   recipeView.addHandlerRender(controlRecipes);
@@ -142,6 +146,6 @@ const init = function () {
   searchView.addHandlerSearch(controlSearchResults);
   paginationView.addHandlerClick(controlPagination);
   addRecipeView.addHandlerUpload(controlAddRecipe);
-  console.log('Welcome!');
+  newFeature();
 };
 init();
